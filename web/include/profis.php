e0337a557207ac937c1334fe43515bd6e24c5a3e
fidels80 <63100124+fidels80@users.noreply.github.com>
2022-02-11 16:55:02 +0100

    aggiuntclasse ditte ma vuota e in config.ini codici

---
+++ web/include/profis.php
@@ -0,0 +1,28 @@
+
+<?php
+class ls
+{
+function creaditta_File($file){
+
+    $D0;
+    $D1;
+    $D2;
+    $D3;
+    $D4;
+    $D5;
+    $D6;
+    $D7;
+    $D8;
+    $DP;
+    $DE;
+    $DR;
+    $DG;
+    $DA;
+    
+
+
+
+}
+}
+
+?>
