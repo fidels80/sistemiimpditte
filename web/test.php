f5c1eefd82a4983244d3ef3bda4d8fe666e5bf0a
fidels80 <63100124+fidels80@users.noreply.github.com>
2022-02-14 11:54:41 +0100

    aggiunto in test.php il select2 per gli ateco con il json

--- web/test.php
+++ web/test.php
@@ -1,13 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 <?php
 
 use Shuchkin\SimpleXLSX;
 
 $vend=str_replace("web","",__DIR__) . 'vendor\shuchkin\simplexlsx\src\SimpleXLSX.php';
 require_once( $vend);
-var_dump($vend);
+//var_dump($vend);
 $ini_array = parse_ini_file("config.ini", true /* will scope sectionally */);
 $ext=$ini_array['EXTENSION'];
-var_dump($ext);
+//var_dump($ext);
 $search='{';
 foreach ($ext['ext'] as $value){
 
@@ -16,10 +26,10 @@ $search=$search  .str_replace('.','',$value).',';
 }
 $search=$search.'}';
 $search=str_replace(',}','}',$search);  
-print_r ($search);;
+//print_r ($search);;
 echo '<br>';
-print_r('.\\'.$ini_array['percorsi']['oripath'] .'*.'. $search);
-var_dump( glob('.\\'.$ini_array['percorsi']['oripath'] .'*.'. $search,GLOB_BRACE));
+//print_r('.\\'.$ini_array['percorsi']['oripath'] .'*.'. $search);
+//var_dump( glob('.\\'.$ini_array['percorsi']['oripath'] .'*.'. $search,GLOB_BRACE));
 
 
 
@@ -40,9 +50,98 @@ foreach($sheets as $index => $name){
 foreach($xlsx->rows(1) as $r){
 
     foreach ($r as $value){
-echo $value.'      ';
+//echo $value.'      ';
     }
-echo '<br>';
+//echo '<br>';
+}
+ 
+
+
+ $data = file_get_contents (__DIR__.'/include/ateco.json');
+ $json = json_decode($data, true);
+ foreach ($json as $key => $value) {
+     if (!is_array($value)) {
+       //   echo $key . '=>' . $value . '<br/>';
+     } else {
+         foreach ($value as $key => $val) {
+         //    echo $key . '=>' . $val . '<br/>';
+         }
+     }
+ }
+
+if (!empty($_POST)){
+echo '<BR>';
+echo '<BR>';
+echo '<BR>';
+var_dump ($_POST['state']);
+}
+else{
+    echo 'banana';
 }
 
 ?>
+
+<script>
+    $(document).ready(function(){
+ //       $.getJSON("atecoid.json", function(data){
+ //          console.log(data);
+ //           console.log(data.data.id); // Prints: Harry
+ //           console.log(data.text); // Prints: 14
+ //       }).fail(function(){
+ //           console.log("An error has occurred.");
+  //      });
+ //       $.getJSON("atecoid.json", function(data){
+ //       $.each(data.features, function(i, feature) {
+ // console.log(feature.properties.id);
+//})
+
+$.getJSON("atecoid.json", function(data){
+    console.log(data);
+    $.each(data, function() {
+    });
+var data2=$.map(data,function(obj){
+    console.log(obj);
+    obj.id=obj.id;
+    obj.text=obj.id +" ||  "+ obj.text;
+    obj.disabled=true;
+    console.log(obj.id.substring(0,1));
+    if ($.isNumeric(obj.id.substring(0,1))) {
+        obj.disabled=false;
+};
+return obj;
+}
+
+)
+
+    $(".js-example-data-array-selected").select2({
+  data: data2
+});
+
+});
+
+        }
+    );
+    </script>
+<form method="post"  action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+<select id='ateco' class="js-example-data-array-selected" style="width: 50%"  name="state">
+<option></option>
+<input name="submit" type="submit" value="Elaborazione Locale" 
+
+/>
+</form>
+ <?php
+  /*
+ foreach ($json as $key => $value) {
+    if (!is_array($value)) {
+        echo '<option value="'.$key.'">'.$key.' ||  '.$value.'</option>';
+    } else {
+        foreach ($value as $key => $val) {
+      echo     '<option value="'.$key.'">'.$key.' ||  '.$val.'</option>';
+        }
+    }
+}*/
+?>
+ 
+</select>
+</body>
+</html>
