7b7dd06082a93d342f7fb6c9af9be2a47db1ce1d
fidels80 <63100124+fidels80@users.noreply.github.com>
2022-02-21 15:59:53 +0100

    prima anagrafica csv e ditta v2

--- web/upload.php
+++ web/upload.php
@@ -3,9 +3,6 @@
 session_start();
 
 ob_start();
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 include(__DIR__.'/include/ls.php');
 include(__DIR__.'/include/PB.php');
 
@@ -23,47 +20,20 @@ for( $i=0 ; $i < $total ; $i++ ) {
 
   //Get the temp file path
   $tmpFilePath = $_FILES['file']['tmp_name'][$i];
-//echo $tmpFilePath.'<br>';
+echo $tmpFilePath.'<br>';
   //Make sure we have a file path
   if ($tmpFilePath != ""){
     //Setup our new file path
     $newFilePath =(__DIR__).$el . $_FILES['file']['name'][$i];
-    //(__DIR__).
-  //  echo $newFilePath.'<br>';
+    echo $newFilePath;
     
     //Upload the file into the temp dir
-    if (move_uploaded_file($tmpFilePath, $newFilePath)) {
-        //   redirect_with_message('The file was uploaded successfully.', FLASH_SUCCESS);
+    if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+     //   redirect_with_message('The file was uploaded successfully.', FLASH_SUCCESS);
       
      //Handle other code here
-    } else {
-     $newFilePath='/app/web/toelab2/'.$_FILES['file']['name'][$i];
-   //  echo $newFilePath.'<br>cc';
-       if (move_uploaded_file($tmpFilePath, $newFilePath)) {
-   
-       }
-        
-    
-    else {
 
-
-      $moved = move_uploaded_file($_FILES["file"]["tmp_name"][$i],
-      '/app/web/toelab2/' . $_FILES['file']['name'][$i] );
-
-      if( $moved ) {
-        echo "Successfully uploaded";         
-      } else {
-   print_r($_FILES["file"]["error"]);
-      }
-
-
-
-
-
-
-
-      $chk=99;}
-    }
+    }else {$chk=99;}
   }
 }
 
@@ -74,9 +44,7 @@ ob_end_flush();
 }
 else
 {
- 
-        die("errore in upload!!!");
-    
-  }
+    die("errore in upload!!!");
+}
 //header('Location: another-php-file.php'); exit();
 ?>
