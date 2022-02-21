5f556079b65d299813ce84c834cd3f3c061df0d8
fidels80 <63100124+fidels80@users.noreply.github.com>
2022-02-21 15:55:08 +0100

    prima anagrafica csv e ditta v2

--- web/upload.php
+++ web/upload.php
@@ -3,6 +3,9 @@
 session_start();
 
 ob_start();
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 include(__DIR__.'/include/ls.php');
 include(__DIR__.'/include/PB.php');
 
