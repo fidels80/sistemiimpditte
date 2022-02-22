61604cec369660613a7c96afadd0424e809c852c
fidels80 <63100124+fidels80@users.noreply.github.com>
2022-02-22 09:58:24 +0100

    prima fase di test produttivo su heroku 1.1a

--- web/elab.php
+++ web/elab.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+//ini_set('display_errors', '1');
+//ini_set('display_startup_errors', '1');
 //error_reporting(E_ALL);
 include(__DIR__.'/include/ls.php');
 include(__DIR__.'/include/PB.php');
